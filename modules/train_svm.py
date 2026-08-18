55c9bbb244d7b30230218381901ab96787d8d0f4
Naitik Parmar <naitikparmar8020@gmail.com>
2026-08-18 23:17:46 +0530

    Model with the accuracy

--- modules/train_svm.py
+++ modules/train_svm.py
@@ -2,6 +2,9 @@ import pandas as pd
 from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split
 import joblib
+from sklearn.metrics import accuracy_score
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
 # Correct import
 from sklearn.svm import SVC
 
@@ -18,8 +21,16 @@ X_train , X_test , y_train , y_test =train_test_split(X,y,test_size=0.2,random_s
 
 # intializing svm model
 print("training SVM model")
-svm_model=SVC(kernel='linear', probability=True)
+svm_model = make_pipeline(StandardScaler(), SVC(kernel='rbf', probability=True, C=10))
 svm_model.fit(X_train , y_train)
+
+# 4. Check the Accuracy on the 20% test data
+predictions = svm_model.predict(X_test)
+accuracy = accuracy_score(y_test, predictions)
+
+
+print(f" MODEL ACCURACY: {accuracy * 100:.2f}%")
+print("----------------------------------------\n")
 
 # save that trained data
 joblib.dump(svm_model , "driver_model.pkl")
